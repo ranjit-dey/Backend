470a599cf94b3f51c0128834adfa229a7cafaf1b
ranjit-dey <ranjitdey05265@gmail.com>
2025-11-25 17:30:01 +0530

    project initial complete

---
+++ 05_projmanagement/src/app.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express from "express";
+const app = express();
+
+/*== BASIC CONFIGURATION ==*/
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+/*== CORS CONFIGURATION ==*/
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+    res.send("about");
+});
+
+app.get("/instagram", (req, res) => {
+    res.send("<h1 style='text-align:center;'>Instagram page</h1>");
+});
+
+export default app;
